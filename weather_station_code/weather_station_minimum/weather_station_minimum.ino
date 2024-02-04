c7759122a8157b592984aaf71fc359a9078eb2f4
fruenger <90471573+fruenger@users.noreply.github.com>
2024-02-04 14:25:08 +0100

    Updated weather_station_minimum
    Add a periodic reset sequence to avoid malfunction of the weather station due to long awake times.

--- weather_station_code/weather_station_minimum/weather_station_minimum.ino
+++ weather_station_code/weather_station_minimum/weather_station_minimum.ino
@@ -1,3 +1,5 @@
+// Version vom 15.12.2023 14:43:00 CET
+
 // used for the I2C interface
 #include <Wire.h>               // library to use I2C (this sensor ONLY uses I2C for communication)
 #include <Adafruit_Sensor.h>    // interface with the BME280 sensor
@@ -22,6 +24,10 @@ const int RecordTime = 1000; //Define Measuring Time (Milliseconds)
 const int SensorPin = 3;  //Define Interrupt Pin (2 or 3 @ Arduino Uno)
 int InterruptCounter;
 float WindSpeed;
+const float conversion_factor = 1.0;
+
+const byte ResetPin = 4; 
+const unsigned long ResetInterval = 3600000;
 
 // light sensor stuff
 UWORD Lux = 0;
@@ -35,6 +41,9 @@ float results[8];
 
 // SETUP Function
 void setup() {
+// set up the reset pin
+digitalWrite(ResetPin, HIGH);
+//pinMode(ResetPin, OUTPUT);
 
 Serial.begin(9600);
 while(!Serial);    // time to get serial running
@@ -47,11 +56,12 @@ pinMode(2,INPUT);
 status = bme.begin(0x76);  
 // You can also pass in a Wire library object like &Wire2
 // status = bme.begin(0x76, &Wire2)
-if (!status) {
-    Serial.println(F("Could not find a valid BME280 sensor, check wiring, address, sensor ID!"));
-    Serial.print(F("SensorID was: 0x")); Serial.println(bme.sensorID(),16);
-    while (1) delay(10);
-}
+
+//if (!status) {
+//    Serial.println(F("Could not find a valid BME280 sensor, check wiring, address, sensor ID!"));
+//    Serial.print(F("SensorID was: 0x")); Serial.println(bme.sensorID(),16);
+//    while (1) delay(10);
+//}
 
 delayTime = 1000;
 
@@ -90,9 +100,9 @@ results[5] = (float) rainAnalogVal * (2 * rainDigitalVal - 1);
 
 
 // Anemomenter part
-WindSpeed = wind_measurement();
+//WindSpeed = wind_measurement();
 
-results[6] = interrupt_rate_measurement();
+results[6] = wind_measurement();
 
 
 // antenna stuff
@@ -104,6 +114,11 @@ for (int i=0; i<8; i++) {
   }
 
 Serial.print("\n");
+
+// Check the run time for whether the Arduino should be resetted.
+if (millis() > ResetInterval) {
+    digitalWrite(ResetPin, LOW);
+  }
 
 }
 
@@ -133,23 +148,13 @@ void printValues() {
     Serial.println();
 }
 
-// take an interrupt rate measurement with the wind sensor
-float interrupt_rate_measurement(void){
-    InterruptCounter = 0;
-    attachInterrupt(digitalPinToInterrupt(SensorPin), countup, RISING);
-    delay(RecordTime);
-    detachInterrupt(digitalPinToInterrupt(SensorPin));
-    float interruptRate = 1000.0 * (float)InterruptCounter / (float)RecordTime;
-    return interruptRate;
-}
-
 // take a measurement with the wind sensor
 float wind_measurement(void){
     InterruptCounter = 0;
     attachInterrupt(digitalPinToInterrupt(SensorPin), countup, RISING);
     delay(RecordTime);
     detachInterrupt(digitalPinToInterrupt(SensorPin));
-    WindSpeed = 1000.0 * (float)InterruptCounter / (float)RecordTime * 2.4;
+    WindSpeed = 1000.0 * (float)InterruptCounter / (float)RecordTime * conversion_factor;
     return WindSpeed;
 }
 
